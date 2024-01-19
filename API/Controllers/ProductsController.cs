e4443d2e3c3405dae42cec48de88bb9746ab0adb
Catherine <catherine.semiletova@gmail.com>
2024-01-19 21:08:47 +0100

    Update the first controller

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
 using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -13,11 +15,20 @@ namespace API.Controllers
         {
             _context = context;
         }
-        
+
         [HttpGet]
-        public string GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            return "Will return all products";
+            var products = await _context.Products.ToListAsync();
+            return products;
+        }
+
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+
         }
     }
 } 
